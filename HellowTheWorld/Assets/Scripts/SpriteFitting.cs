f3e6118a24fe59e57d2221145e3042885a79b194
兵頭智輝 <tomoki.hyodo@hautecouture.jp>
2019-07-05 21:39:25 +0900

    ステージアセット追加。テストスプライト追加。

---
+++ HellowTheWorld/Assets/Scripts/SpriteFitting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteFitting : MonoBehaviour
+{
+    Sprite sprite;
+
+    [SerializeField]
+    FittingMode mode;
+
+    float rate;
+
+    enum FittingMode
+    {
+        Width,
+        Height
+    };
+
+    private void Awake()
+    {
+        sprite = GetComponent<Sprite>();
+        if (mode == FittingMode.Width)
+        {
+            float width = sprite.bounds.size.x;
+            rate = Screen.width / width;
+            transform.localScale = new Vector3(rate, 1, 1);
+        }
+        else
+        {
+            float height = sprite.bounds.size.y;
+            rate = Screen.height / height;
+            transform.localScale = new Vector3(1, rate, 1);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ HellowTheWorld/Assets/Scripts/ThrowItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowItem : MonoBehaviour
+{
+    float span = 0.5f;
+
+    [SerializeField]
+    private GameObject[] Items;
+
+    [SerializeField]
+    private Transform parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        span -= Time.deltaTime;
+        if (span <= 0)
+        {
+            span = 0.5f;
+            Throw();
+        }
+    }
+
+    void Throw()
+    {
+        GameObject obj = Instantiate(Items[Random.Range(0, Items.Length-1)],parent);
+        obj.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 2f);
+    }
+}
